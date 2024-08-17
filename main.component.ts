ded61f4eaf34f41f226fa4f29f2792f2f75f1fa3
mahmoudg.negm <mahmoudg.negm@outlook.com>
2024-08-17 15:12:36 +0300

    1

--- main.component.ts
+++ main.component.ts
@@ -13,26 +13,15 @@ export class MainComponent {
    checkUsername() {
     this.isValidbtn = this.userName.trim() !== '';
   }
-
-
-
   onResetUsername(){
 this.userName=''; 
 this.checkUsername() ;
 this.logButtonClick('Reset Username'); 
-
-
   }
-
-    
   onToggleParagraph (){
 this .showParagraph= !this.showParagraph; 
 this.logButtonClick('Toggle Paragraph'); 
-
-
-
   }
-
   logButtonClick(buttonName: string) {
     const timestamp = new Date().toLocaleString();
     this.clickLogs.push(`${timestamp}: ${buttonName} button clicked`); 
